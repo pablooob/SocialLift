307deb704bb230b720bc975ece959d0276f188e8
Juan Pablo Bueno Fuentes <JPBUEFUE@upv.edu.es>
2024-02-21 15:34:03 +0100

    Modelos serie completo

--- src/main/java/com/SocialLift/SocialLift/Models/Serie.java
+++ src/main/java/com/SocialLift/SocialLift/Models/Serie.java
@@ -1,4 +1,23 @@
 package com.SocialLift.SocialLift.Models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Serie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String numeroSerie;
+
+    @Column(nullable = false)
+    private String peso;
+
+    @Column(nullable = false)
+    private String numeroRepeticiones;
+
 }
--- src/main/java/com/SocialLift/SocialLift/Models/Usuario.java
+++ src/main/java/com/SocialLift/SocialLift/Models/Usuario.java
@@ -15,7 +15,7 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idUsuario;
 
     @Column(nullable = false)
     private String nombre;
