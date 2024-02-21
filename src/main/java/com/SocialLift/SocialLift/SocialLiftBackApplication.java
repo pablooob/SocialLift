13044d1f32cfcb27a81662763f4343f8ded0a4df
Juan Pablo Bueno Fuentes <JPBUEFUE@upv.edu.es>
2024-02-21 15:03:19 +0100

    Innitial Commit

---
+++ src/main/java/com/SocialLift/SocialLift/SocialLiftBackApplication.java
@@ -0,0 +1,13 @@
+package com.SocialLift.SocialLift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLiftBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLiftBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SocialLift/SocialLift/SocialLiftBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SocialLift.SocialLift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLiftBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
